60d517057f331b18bc274c446e082eaacd01bf53
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-25 14:05:05 -0400

    got increment in console

---
+++ src/components/Start.js
@@ -0,0 +1,52 @@
+/* Start button should have a function that increases per Second
+setInterval seems like a good method to use for this  
+Step1 Get something to show on App.js - DONE
+Step2 Create a function that increments - DONE
+Step2b be able to console.log something - DONE
+Looking at https://reactjs.org/docs/faq-functions.html, it seems like functions go before the render method
+Step3 Show the incremental Number on screen
+Step3b Show a number on screen - DONE
+Looking at https://www.youtube.com/watch?v=4ORZ1GmjaMc, learned more about state and how it works*/
+
+
+import React, { Component } from 'react';
+
+/* let initialNumber = 0;
+let buttonClicked = null;
+let keepTime
+
+function incrementTimer() {
+  let secondsPlus = initialNumber++;
+  console.log(secondsPlus);
+}
+
+function startTimer() {
+    keepTime = setInterval(incrementTimer, 1000);
+    buttonClicked = true;
+    console.log(buttonClicked);
+  }
+ */
+class Start extends Component {
+    
+    constructor(){
+        super()
+        this.state = {
+            timer: 0
+        }
+    }
+    
+   /*  startTimer() {
+        this.setState({
+            timer: 
+        })
+      } */
+
+    render() { 
+        return (<div>
+            <h1> {this.state.timer} </h1>
+            <button className="start" onClick={() => this.startTimer()}>START</button>
+        </div>  );
+    }
+}
+ 
+export default Start;
