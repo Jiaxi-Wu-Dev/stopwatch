70e56956b0e5318fad820ba69ff44769c955f2d5
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-26 17:16:34 -0400

    number increments working with start

--- src/components/Pause.js
+++ src/components/Pause.js
@@ -1,3 +1,5 @@
+// Create a pause component that pauses the count from start.js
+
 import React, { Component } from 'react';
 
 class Pause extends Component {
--- src/components/Start.js
+++ src/components/Start.js
@@ -4,9 +4,10 @@ Step1 Get something to show on App.js - DONE
 Step2 Create a function that increments - DONE
 Step2b be able to console.log something - DONE
 Looking at https://reactjs.org/docs/faq-functions.html, it seems like functions go before the render method
-Step3 Show the incremental Number on screen
+Step3 Show the incremental Number on screen - DONE
 Step3b Show a number on screen - DONE
-Looking at https://www.youtube.com/watch?v=4ORZ1GmjaMc, learned more about state and how it works*/
+Looking at https://www.youtube.com/watch?v=4ORZ1GmjaMc, learned more about state and how it works
+https://github.com/airbnb/react-sketchapp/issues/248 for setState and setInterval */
 
 import React, { Component } from "react";
 
@@ -14,23 +15,29 @@ class Start extends Component {
   constructor() {
     super();
     this.state = {
+      time: 0,
       timer: 0
     };
+    this.incrementOne = this.incrementOne.bind(this);
   }
 
-  incrementOne = () => {
+  incrementOne() {
     this.setState({
-      timer: setInterval(() => {
+      time: setInterval(() => {
         this.setState({ timer: this.state.timer + 1 });
       }, 1000)
     });
-  };
+  }
 
   render() {
     return (
       <div>
         <h1> {this.state.timer} </h1>
-        <button className="start" onClick={() => this.incrementOne()}>
+ 
+        <button
+          className="start"
+          onClick={this.incrementOne.bind(this, "start")}
+        >
           START
         </button>
       </div>
